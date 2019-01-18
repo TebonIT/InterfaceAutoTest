cf37405afacd81ffd36ba26073ed0208435a1484
lwjzy160 <lwjzy160@163.com>
2019-01-18 17:34:16 +0800

    commit

---
+++ Hello/src/com/tebon/demo.java
@@ -0,0 +1,17 @@
+package com.tebon;
+
+import java.io.IOException;
+
+public class demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+}
